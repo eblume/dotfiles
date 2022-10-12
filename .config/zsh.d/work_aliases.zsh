a3951adcaf1dc53cd441f33a2f4ca63222043290
Erich Blume <blume.erich@gmail.com>
2022-10-12 09:27:28 -0700

    more tooling for work

--- .config/zsh.d/work_aliases.zsh
+++ .config/zsh.d/work_aliases.zsh
@@ -1,5 +1,15 @@
 #!/bin/zsh
+# Ok so the file was misnamed, sometimes I do functions here too
 
-alias fly-devcourse="env ASDF_CONCOURSE_VERSION=5.7.0 fly -t devcourse"
 
-alias orc-run="docker run -it -v ~/code/work:/root/work -v ~/.gitconfig:/root/.gitconfig -v ~/.chef:/root/.chef --platform=linux/amd64 docker-chaos.ilabs.io/blackwidow/centos7-orc"
+fly-devcourse() {
+    env ASDF_CONCOURSE_VERSION=5.7.0 fly -t devcourse $@
+}
+
+orcrun() {
+    docker run -it -v ~/code/work:/root/work -v ~/.gitconfig:/root/.gitconfig -v ~/.chef:/root/.chef --platform=linux/amd64 docker-chaos.ilabs.io/blackwidow/centos7-orc $@
+}
+
+myrun() {
+    docker run -it -v ~/code/work:/root/work -v ~/.gitconfig:/root/.gitconfig -v ~/.chef:/root/.chef --platform=linux/amd64 mydocker $@
+}
