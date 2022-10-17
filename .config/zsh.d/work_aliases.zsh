bd91bffe6a90a5715f31d7c9b4ab494dffd4190c
Erich Blume <blume.erich@gmail.com>
2022-10-17 11:23:30 -0700

    prodcourse

--- .config/zsh.d/work_aliases.zsh
+++ .config/zsh.d/work_aliases.zsh
@@ -6,6 +6,14 @@ fly-devcourse() {
     env ASDF_CONCOURSE_VERSION=5.7.0 fly -t devcourse $@
 }
 
+fly-prodcourse() {
+    env ASDF_CONCOURSE_VERSION=5.7.0 fly -t prodcourse $@
+}
+
+fly-govcourse() {
+    env ASDF_CONCOURSE_VERSION=5.7.0 fly -t govcourse $@
+}
+
 orcrun() {
     docker run -it -v ~/code/work:/root/work -v ~/.gitconfig:/root/.gitconfig -v ~/.chef:/root/.chef --platform=linux/amd64 docker-chaos.ilabs.io/blackwidow/centos7-orc $@
 }
