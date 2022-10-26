34408a11fe495ff8c03a9e4bd3a20d497a940442
Erich Blume <blume.erich@gmail.com>
2022-10-26 12:43:03 -0700

    Switch to new ilodevops

--- .config/zsh.d/work_aliases.zsh
+++ .config/zsh.d/work_aliases.zsh
@@ -14,30 +14,4 @@ fly-govcourse() {
     env ASDF_CONCOURSE_VERSION=5.7.0 fly -t govcourse $@
 }
 
-orcrun() {
-    docker run -it -v ~/code/work:/root/work -v ~/.gitconfig:/root/.gitconfig -v ~/.chef:/root/.chef --platform=linux/amd64 docker-chaos.ilabs.io/blackwidow/centos7-orc $@
-}
-
-ilodevops-dev() {
-    ilodevops-core ilodevops-dev $@
-}
-
-ilodevops-prod() {
-    ilodevops-core ilodevops-prod $@
-}
-
-ilodevops-gov() {
-    ilodevops-core ilodevops-gov $@
-}
-
-ilodevops-core() {
-    if [ -z `ilodevops-compose ps -q $1` ] || [ -z `docker ps -q --no-trunc | grep $(ilodevops-compose ps -q $1)` ]; then
-        echo "service '$1' is not running. Have you run 'ilodevops-compose up'?"
-    else
-        ilodevops-compose run $@
-    fi
-}
-
-ilodevops-compose() {
-    docker compose -f ~/code/work/eblume/ilodevops/compose.yaml $@
-}
+export PATH="$PATH:$HOME/code/work/eblume/ilodevops/bin"
