b6799f124e81b64d94d6534b7fc45f725d2831bb
Erich Blume <blume.erich@gmail.com>
2023-05-06 09:39:09 -0700

    no wrapped zellij via ssh

--- .config/zsh.d/zellij.zsh
+++ .config/zsh.d/zellij.zsh
@@ -1,5 +1,7 @@
-### Automatically box in to a new zellij session if one doesn't exist
-eval "$(zellij setup --generate-auto-start zsh)"
+### Automatically box in to a new zellij session if one doesn't exist and we aren't SSHing
+if [ -z "$SSH_CONNECTION" ]; then
+  eval "$(zellij setup --generate-auto-start zsh)"
+fi
 
 ### Helpful aliases, originally from the --generate-completion zsh output, moved here
 function zr () { zellij run --name "$*" -- zsh -ic "$*";}
