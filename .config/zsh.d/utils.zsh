a8ee658640c56d92c95afb6d30e9f5264b2b70f3
Erich Blume <blume.erich@gmail.com>
2023-06-10 11:36:11 -0700

    minor

---
+++ .config/zsh.d/utils.zsh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+# Various utility functions
+
+function my_macname() {
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: my_macname get | set NEW_NAME"
+    return 1
+  fi
+
+  local action="$1"
+  local new_name="$2"
+
+  if [[ "${action}" == "get" ]]; then
+    echo "ComputerName: $(scutil --get ComputerName 2>/dev/null)"
+    echo "LocalHostName: $(scutil --get LocalHostName 2>/dev/null)"
+    echo "HostName: $(scutil --get HostName 2>/dev/null)"
+  elif [[ "${action}" == "set" ]]; then
+    if [[ -z "${new_name}" ]]; then
+      echo "Please provide a new name."
+      return 1
+    fi
+
+    if ! [[ "${new_name}" =~ ^[a-zA-Z0-9-]+$ ]]; then
+      echo "Invalid name. Name must contain only alphanumeric characters and hyphens."
+      return 1
+    fi
+
+    sudo scutil --set ComputerName "${new_name}"
+    sudo scutil --set LocalHostName "${new_name}"
+    sudo scutil --set HostName "${new_name}"
+    echo "All hostnames have been set to ${new_name}."
+  else
+    echo "Invalid action. Use 'get' or 'set'."
+    return 1
+  fi
+}
+
