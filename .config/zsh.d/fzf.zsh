df1f2078f44b3cefb6246a2e095d13270f2a0554
Erich Blume <blume.erich@gmail.com>
2023-07-19 02:47:51 -0700

    fzf use ripgrep

--- .config/zsh.d/fzf.zsh
+++ .config/zsh.d/fzf.zsh
@@ -13,5 +13,8 @@ fi
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # Use ripgrep
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# -----------
+if command -v rg &> /dev/null; then
+  export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git/*'"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
