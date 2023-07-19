6f6c7f1987f01ae03fa1c57f2fe6d4ef08cf99fd
Erich Blume <blume.erich@gmail.com>
2023-07-18 19:56:55 -0700

    Lets turn off zellij autostart

--- .config/zsh.d/zellij.zsh
+++ .config/zsh.d/zellij.zsh
@@ -1,7 +1,8 @@
 ### Automatically box in to a new zellij session if one doesn't exist and we aren't SSHing
-if [ -z "$SSH_CONNECTION" ]; then
-  eval "$(zellij setup --generate-auto-start zsh)"
-fi
+## (Disabled for now - it's a bit too aggressive)
+# if [ -z "$SSH_CONNECTION" ]; then
+#   eval "$(zellij setup --generate-auto-start zsh)"
+# fi
 
 ### Helpful aliases, originally from the --generate-completion zsh output, moved here
 function zr () { zellij run --name "$*" -- zsh -ic "$*";}
