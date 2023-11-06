b0ca52b9fe1bfa0c8063ef6596049700bcf25433
Erich Blume <blume.erich@gmail.com>
2023-11-06 14:36:15 -0800

    Retire my_vm and my_log

--- .config/zsh.d/utils.zsh
+++ .config/zsh.d/utils.zsh
@@ -95,34 +95,6 @@ function my_llm() {
   fi
 }
 
-function my_log() {
-  # Use nb to add a diary entry. If it's the first entry of the day, use a full date line, otherwise use an abbreviated
-  # one.
-  long_form_date=$(date '+%A, %B %d, %Y')
-  short_form_date=$(date '+%H:%M')
-  calendar_date=$(date '+%Y-%m-%d')
-  shortmsg=$'\n'"$*"
-
-  # First, create all preamble for this entry including the file itself if needed
-  entries=$(nb list --type=log.md "${calendar_date}.log.md")
-  if [ $? -eq 0 ]; then
-    # entries were found, so use nb edit
-    nb edit "${calendar_date}.log.md" --content "## ${short_form_date}${shortmsg}"
-  else
-    # entries were NOT found, so use nb add
-    nb add "${calendar_date}.log.md" --title "${long_form_date}" --content "## ${short_form_date}${shortmsg}" --type=log.md
-  fi
-
-  # If no args were passed, open the file in the editor
-  if [ $# -eq 0 ]; then
-    # Why do this and not just use --edit? Because --edit _locks nb systemwide_. wtf?
-    nb open "${calendar_date}.log.md"
-  fi
-
-  # Manually sync because I've turned off auto-sync to prevent ssh credential issues
-  nb sync # (nbo 118)
-}
-
 function my_idea() {
   # Use nb to add an idea entry
   if [ $# -eq 0 ]; then
@@ -161,102 +133,6 @@ EOF
 
 function my_logsum() {
   my_summarize --type=log.md --sort --reverse
-}
-
-function my_transcribe() {
-  # my_transcribe FILE
-  # Transcribe an audio file to text using whisper.cpp
-  # This function will, if needed, clone and build whisper.cpp and download a good model
-  #
-  # If ~/code/whisper.cpp does not exist, clone it
-  codedir=~/code/3rd/whisper.cpp
-  if [ ! -d $codedir ]; then
-    git clone --quiet git@github.com:ggerganov/whisper.cpp.git $codedir
-  fi
-  cd $codedir
-
-  git pull --quiet
-  bash ./models/download-ggml-model.sh base.en > /dev/null 2>&1
-  make > /dev/null 2>&1
-  
-  outfile="$(mktemp).wav"
-  ffmpeg -i "$1" -ar 16000 -hide_banner -loglevel warning -nostats $outfile
-
-  ./main -nt -f $outfile 2>/dev/null | grep -v '^$' | sed 's/^[ \t]*//'
-
-  rm $outfile
-  cd ~-
-}
-
-function my_vm() {
-  # Batch process cloud-synced JustPressRecord voice memos
-  # For each memo found, it will be transcribed and added to that day's log
-  # If that day's log doesn't exist, it will be created
-  # If it contains a task, it will be added to todoist
-  # TODO: the voice memo should be archived (it is not currently)
-  # The voice memo will then be deleted.
-  # Thus, this function will ultimately move voice memos in to NB via transcription.
-
-  # First, sync, because this is useless if its not synced
-  nb sync
-  
-  clouddir="$HOME/Library/Mobile Documents/iCloud~com~openplanetsoftware~just-press-record/Documents"
-
-  for file in $clouddir/*/*.m4a; do
-    echo "Processing $file"
-    date="$(basename "$(dirname "$file")")"
-    thetime="$(date -j -f "%H-%M-%S" "$(basename "$file" | sed 's/\.m4a$//')" "+%H:%M")"
-    longformat="+%A, %B %d, %Y"
-    long_form_date="$(date -j -f "%Y-%m-%d" "$date" "$longformat")"
-    transcription="$(my_transcribe "$file")"
-    # Weird newline hack, to get the log entries spaced properly
-    transcript=$'\n'$'\n'"$transcription"
-    echo
-    echo "Transcription"
-    echo "$transcription"
-    echo
-
-    # TODO this UPSERT pattern is dumb
-    add_or_edit=$(if nb list --type=log.md "$date.log.md" > /dev/null 2>&1; then echo "edit"; else echo "add"; fi)
-    
-    # Record the voice memo in the log
-    nb $add_or_edit "$date.log.md" --title "$long_form_date" --type=log.md --content "## $thetime (Voice Memo)$transcript"
-    nb sync # (nbo 118)
-
-    # TODO archive
-    rm "$file"
-
-    # Extract TODOs from the voice memo
-    response="$(my_llm -s "Please extract any tasks from the voice memo below, one task per line. If there are no tasks, just say 'none'. Be concise, don't respond with any chatter." <<< $transcription)"
-    if [ -z "$response" ]; then
-      echo "Task-extraction is empty or malformed. Aborting process. (This is a bug.)"
-      return 1
-    fi
-
-    # Truncate to 1000 characters because gpt4 is expensive
-    response="${response:0:1000}"
-
-    # Clean response in case of checking for 'none'
-    response_clean="$(echo "$response" | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | xargs)"
-
-    if [[ "$response_clean" == "none" ]]; then
-      # Do nothing as there are no tasks, just continue with the next file
-      echo "No tasks found in $file"
-      continue
-    fi
-
-    # Split on newline into an array the zsh way
-    tasks=("${(@f)response}")
-    echo "Found ${#tasks[@]} tasks in $file"
-
-    # Iterate over each task and pass them to the 'my_todo' function
-    for task in "${tasks[@]}"; do
-      # Trim markdown formatting from the beginning of the task if it's there
-      task_clean=$(echo "$task" | sed 's/^ *- *//')
-      my_todo "$task_clean"
-    done
-  done
-
 }
 
 function my_project {
