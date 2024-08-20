b19e4c865070b316d9603c3ecc25204a3c1ad7c8
Erich Blume <erich.blume@worldpay.com>
2024-08-20 09:43:42 -0700

    Fix rds hostname

--- modules/work/functions/payrix-rds-pci.fish
+++ modules/work/functions/payrix-rds-pci.fish
@@ -1,4 +1,4 @@
-set -f host "http://prod-payrix-replica-3.cvzqqve8hxw0.us-east-1.rds.amazonaws.com/"
+set -f host "prod-payrix-replica-3.cvzqqve8hxw0.us-east-1.rds.amazonaws.com"
 set -f psql_command "psql -h $host -d payrix -U payrixroot -p 5432"
 echo $psql_command | pbcopy
 echo "psql command (copied to clipboard): $psql_command"
