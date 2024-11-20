9ea5be134641ed1191d8e768b549cf544702ef8d
Erich Blume <725328+eblume@users.noreply.github.com>
2024-11-20 14:39:29 +0000

    Remove nix-index creation

--- modules/common/shell/nixpkgs.nix
+++ modules/common/shell/nixpkgs.nix
@@ -66,19 +66,6 @@
       enableFishIntegration = true;
     };
 
-    # Create nix-index if doesn't exist
-    home.activation.createNixIndex =
-      let
-        cacheDir = "${config.homePath}/.cache/nix-index";
-      in
-      lib.mkIf config.home-manager.users.${config.user}.programs.nix-index.enable (
-        config.home-manager.users.${config.user}.lib.dag.entryAfter [ "writeBoundary" ] ''
-          if [ ! -d ${cacheDir} ]; then
-              $DRY_RUN_CMD ${pkgs.nix-index}/bin/nix-index -f ${pkgs.path}
-          fi
-        ''
-      );
-
     # Set automatic generation cleanup for home-manager
     nix.gc = {
       automatic = config.nix.gc.automatic;
