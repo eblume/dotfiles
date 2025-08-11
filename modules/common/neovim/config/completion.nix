0226a5dab7ee904148f2231a14277220bfe53a68
Erich Blume <erich.blume@worldpay.com>
2025-08-11 10:25:01 -0700

    Hopefully fix blink config

--- modules/common/neovim/config/completion.nix
+++ modules/common/neovim/config/completion.nix
@@ -7,25 +7,25 @@
     pkgs.vimPlugins.friendly-snippets
   ];
 
-  use.blink-cmp.setup = dsl.callWith {
+  use."blink.cmp".setup = dsl.callWith {
     # https://cmp.saghen.dev/configuration/general.html
-    opts = {
-      keymap.preset = "default"; # like neovim default, ctrl-y to accept
-      appearance.nerd_font_variant = "mono";
-      completion.documentation.auto_show = true;
-      sources.default = [
-        "lsp"
-        "path"
-        "snippets"
-        "buffer"
-      ];
-      fuzzy.implementation = "prefer_rust_with_warning";
-      signature.enabled = true;
-    };
-    dependencies =
+    keymap =
       dsl.rawLua # lua
         ''
-          { 'L3MON4D3/LuaSnip', version = 'v2.*' }
+          {
+            preset = "default", -- like neovim default, <C-y> to accept
+            ["<C-d>"] = { "show", "show_documentation", "hide_documentation"}, -- <C-space> def conflicts with hammerspoon
+          }
         '';
+    appearance.nerd_font_variant = "mono";
+    completion.documentation.auto_show = true;
+    sources.default = [
+      "lsp"
+      "path"
+      "snippets"
+      "buffer"
+    ];
+    fuzzy.implementation = "prefer_rust_with_warning";
+    signature.enabled = true;
   };
 }
