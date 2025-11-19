78c286c1bac6ef5e793c12ade4f8f7ce20213ab8
Erich Blume <blume.erich@gmail.com>
2025-11-18 18:46:37 -0800

    gitsigns

--- dot_config/nvim/lua/plugins/scm.lua
+++ dot_config/nvim/lua/plugins/scm.lua
@@ -1,4 +1,40 @@
 return {
 	{ "tpope/vim-fugitive" },
 	{ "tpope/vim-rhubarb" },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			local gitsigns = require("gitsigns")
+			vim.keymap.set("n", "<Leader>gB", gitsigns.blame_line)
+			vim.keymap.set("n", "<Leader>gp", gitsigns.preview_hunk)
+			vim.keymap.set("v", "<Leader>gp", gitsigns.preview_hunk)
+			vim.keymap.set("n", "<Leader>gd", gitsigns.diffthis)
+			vim.keymap.set("v", "<Leader>gd", gitsigns.diffthis)
+			vim.keymap.set("n", "<Leader>rgf", gitsigns.reset_buffer)
+			vim.keymap.set("v", "<Leader>hs", gitsigns.stage_hunk)
+			vim.keymap.set("n", "<Leader>hr", gitsigns.reset_hunk)
+			vim.keymap.set("v", "<Leader>hr", gitsigns.reset_hunk)
+
+			-- Navigation
+			vim.keymap.set("n", "]g", function()
+				if vim.wo.diff then
+					return "]g"
+				end
+				vim.schedule(function()
+					gitsigns.next_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true })
+
+			vim.keymap.set("n", "[g", function()
+				if vim.wo.diff then
+					return "[g"
+				end
+				vim.schedule(function()
+					gitsigns.prev_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true })
+		end,
+	},
 }
