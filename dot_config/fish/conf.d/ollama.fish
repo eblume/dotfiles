34a1fff737c0050e787b926a524465f162d96df0
Erich Blume <blume.erich@gmail.com>
2026-03-03 09:42:28 -0800

    update ollama again

--- dot_config/fish/conf.d/ollama.fish
+++ dot_config/fish/conf.d/ollama.fish
@@ -1,3 +1,3 @@
 # Local subnet access for non-tailnet devices
-set -gx OLLAMA_HOST http://192.168.1.21:30434
-set -gx OLLAMA_API_BASE http://192.168.1.21:30434
+set -gx OLLAMA_HOST http://192.168.1.21
+set -gx OLLAMA_API_BASE http://192.168.1.21
