878083750230733704464f1b907d443ac75adfbd
Erich Blume <blume.erich@gmail.com>
2026-03-30 10:41:52 -0700

    remove nix aliases

--- dot_config/fish/conf.d/ollama.fish
+++ dot_config/fish/conf.d/ollama.fish
@@ -1,3 +1,2 @@
-# Local subnet access for non-tailnet devices
-set -gx OLLAMA_HOST http://192.168.1.21
-set -gx OLLAMA_API_BASE http://192.168.1.21
+set -gx OLLAMA_HOST https://ollama.ops.eblu.me
+set -gx OLLAMA_API_BASE https://ollama.ops.eblu.me
