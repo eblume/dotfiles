a7cfaa348881337c8f9104fa1d2be864c2320a2d
Erich Blume <blume.erich@gmail.com>
2026-04-19 09:15:02 -0700

    Fix pypi.fish

--- dot_config/fish/conf.d/pypi.fish
+++ dot_config/fish/conf.d/pypi.fish
@@ -1,6 +1,8 @@
-# Use private PyPI proxy (devpi on indri) when enabled
+# Use private PyPI indexes when enabled
 # Toggle with: devpi on | devpi off | devpi status
 if test "$DEVPI_ENABLED" = 1
     set -gx PIP_INDEX_URL "https://pypi.ops.eblu.me/root/pypi/+simple/"
-    set -gx UV_INDEX_URL "https://pypi.ops.eblu.me/root/pypi/+simple/"
+    set -gx UV_INDEX "forgejo=https://forge.eblu.me/api/packages/eblume/pypi/simple/"
+    set -gx UV_DEFAULT_INDEX "https://pypi.ops.eblu.me/root/pypi/+simple/"
+    set -e UV_INDEX_URL  # clear legacy var to avoid conflict
 end
